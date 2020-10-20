7b65f2bf594756f0f8be1ae8f7eda3ad9c9fb554
LaloSoftware <eduardo.lemus.laguna@gmail.com>
2020-10-19 22:06:25 -0500

    Inclusión de la librería de Socket.io

--- classes/server.ts
+++ classes/server.ts
@@ -1,17 +1,36 @@
 import express from "express"
 import {SERVER_PORT} from '../global/environment';
+import socketIO from "socket.io";
+import http from 'http'
 
 export default class Server{
+    private static _instance: Server;
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
-    constructor(){
+    private constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        this.escicharSockets();
+    }
+
+    public static get instace() {
+        return this._instance || (this._instance = new this());
+    }
+
+    private escicharSockets(){
+        console.log("Escucahando conexiones");
+        this.io.on("connection", cliente => {
+            console.log("Cliente conectado");
+        });
     }
 
     start(){
-        this.app.listen(this.port, ()=>{
+        this.httpServer.listen(this.port, ()=>{
             console.log(`Corriendo en puerto ${this.port}`);
         });
     }
