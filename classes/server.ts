33efda0e53473d199d0e6acb3c7538fe31b8d465
LaloSoftware <eduardo.lemus.laguna@gmail.com>
2020-10-19 23:07:24 -0500

    Uso de lbrería de sockets y separación de eventos

--- classes/server.ts
+++ classes/server.ts
@@ -1,7 +1,8 @@
 import express from "express"
 import {SERVER_PORT} from '../global/environment';
 import socketIO from "socket.io";
-import http from 'http'
+import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server{
     private static _instance: Server;
@@ -26,6 +27,8 @@ export default class Server{
         console.log("Escucahando conexiones");
         this.io.on("connection", cliente => {
             console.log("Cliente conectado");
+            //desconectar
+            socket.deconectar(cliente);
         });
     }
 
